2bfad47f4bd9c7cae80f097193ed860186b8d33d
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-03-23 23:14:56 -0400

    create effect hook for calling API

--- lab5/src/App.jsx
+++ lab5/src/App.jsx
@@ -1,7 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
+  const [list, setList] = useState(null);
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        `https://min-api.cryptocompare.com/data/all/coinlist?&${API_KEY}`
+      );
+
+      const json = await response.json();
+      console.log(json);
+    }
+    fetchAllCoinData().catch(console.error);
+  }, []);
 
   return (
     <div>
